fc955fa182e9d37077267abf357db54512ea3426
alainMH <alainkatayi01@gmail.com>
2025-01-11 02:29:16 +0100

    Création de la deuxiéme applications events

--- ManageEvent/accounts/urls.py
+++ ManageEvent/accounts/urls.py
@@ -4,5 +4,8 @@ from  .import views
 app_name = 'accounts'
 
 urlpatterns = [
-    path('', views.index, name = 'index')
+    path('', views.index, name = 'index'),
+    path('login/', views.login_user, name = 'login_user'),
+    path('register/', views.register_user, name = 'register_user'),
+    path('success/', views.success, name = 'success')
 ]
--- ManageEvent/accounts/views.py
+++ ManageEvent/accounts/views.py
@@ -1,5 +1,38 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from .forms import CustomUserCreationForm
 
 # Create your views here.
 def index(request):
     return render(request, 'accounts/index.html')
+
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST ['username']
+        password = request.POST['password']
+        user = authenticate(request, username = username , password = password)
+        if user is not None:
+            login(request, user)
+            return redirect ('accounts:success')
+        else:
+            messages.info (request, 'Password or username is no correct')
+    
+    form = AuthenticationForm()
+    return render (request, 'accounts/login.html', {'form':form})
+
+def register_user(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('accounts:success')
+    else:
+        form = CustomUserCreationForm()
+        return render(request, 'accounts/register.html', {'form': form})
+
+
+def success(request):
+    return render(request, 'accounts/success.html')
